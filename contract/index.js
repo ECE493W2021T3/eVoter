1f7c98409af45dbadcd14320a07cfadee734a36b
Liyao Jiang <liyao1@ualberta.ca>
2021-03-25 11:54:52 -0600

    rename smart contract

--- contract/index.js
+++ contract/index.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-const MyAssetContract = require('./lib/my-asset-contract');
+const MyAssetContract = require('./lib/evoterContract.js');
 
 module.exports.MyAssetContract = MyAssetContract;
 module.exports.contracts = [ MyAssetContract ];
