2896bcc14fecce5b855b74eb13258b99ff0196a4
Trishia Gatilogo <gatilogo@ualberta.ca>
2021-03-24 18:15:54 -0600

    #31 deal case where poll type was changed from survey to election

--- frontend/src/app/components/new-poll/new-poll.component.ts
+++ frontend/src/app/components/new-poll/new-poll.component.ts
@@ -15,11 +15,13 @@ import { ACCESS_LEVELS, COMMON, POLL_TYPES, QUESTION_TYPES } from 'src/app/helpe
 export class NewPollComponent implements OnInit {
     public pollConfigForm: FormGroup;
     public questionForm: FormGroup;
-
     public pollTypes = POLL_TYPES;
     public accessLevels = ACCESS_LEVELS;
     public questionTypes = QUESTION_TYPES;
     public minDate = new Date();
+
+    private oldSelectedPollType: string;
+    private selectedPollType: string;
 
     constructor(
         private formBuilder: FormBuilder) { }
@@ -45,10 +47,42 @@ export class NewPollComponent implements OnInit {
     get pcf() { return this.pollConfigForm.controls; }
     get qfa() { return this.questionForm.controls.questions as FormArray; }
 
+    onPollTypeChange(event) {
+        this.selectedPollType = event.value;
+
+        // If initial poll type is empty, add a question
+        if (this.oldSelectedPollType == null) {
+            this.addQuestion();
+            if (this.selectedPollType == COMMON.pollType.election) {
+                this.addChoice(this.qfa.controls[0]);
+            }
+        }
+        // If poll type was changed from survey to election, remove all short answer questions and questions with empty types
+        else if (this.oldSelectedPollType == COMMON.pollType.survey && this.selectedPollType == COMMON.pollType.election) {
+            const indices = this.qfa.value
+                .map((item, index) => item.questionType == COMMON.questionType.shortAnswer || item.questionType == '' ? index : null)
+                .filter(x => x != null)
+                .sort((a, b) => b - a); // sort indices in descending order
+
+            indices.forEach(index => {
+                this.qfa.removeAt(index);
+            });
+        }
+
+        this.oldSelectedPollType = this.selectedPollType;
+    }
+
+    onQuestionTypeChange(event, item) {
+        if (event.value == COMMON.questionType.multipleChoice && item.controls.choices.length == 0) {
+            this.addChoice(item);
+        }
+    }
+
     addQuestion() {
+        const questionType = this.selectedPollType == COMMON.pollType.election ? COMMON.questionType.multipleChoice : '';
         this.qfa.push(this.formBuilder.group({
             question: ['', Validators.required],
-            questionType: ['', Validators.required],
+            questionType: [questionType, Validators.required],
             choices: new FormArray([])
         }));
     }
@@ -67,16 +101,5 @@ export class NewPollComponent implements OnInit {
     deleteChoice(item, index) {
         var choices = item.controls.choices as FormArray;
         choices.removeAt(index);
-    }
-
-    onQuestionTypeChange(event, item) {
-        if (event.value == COMMON.questionType.multipleChoice && item.controls.choices.length == 0) {
-            this.addChoice(item);
-        }
-    }
-
-    test() {
-        console.log(this.pcf);
-        console.log(this.qfa);
     }
 }
