3dec120b22546236f2cd750fad7c8283c450fe23
Trishia Gatilogo <gatilogo@ualberta.ca>
2021-03-26 00:23:49 -0600

    add error state matcher for confirming passwords

--- frontend/src/app/components/signup-page/signup-page.component.ts
+++ frontend/src/app/components/signup-page/signup-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import { MyErrorStateMatcher } from 'src/app/helpers/default.error-matcher';
 
 @Component({
     selector: 'app-signup-page',
@@ -11,6 +11,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class SignupPageComponent implements OnInit {
     public registrationForm: FormGroup;
+    public matcher = new MyErrorStateMatcher();
 
     constructor(
         private formBuilder: FormBuilder,
@@ -21,12 +22,14 @@ export class SignupPageComponent implements OnInit {
         this.registrationForm = this.formBuilder.group({
             name: ['', Validators.required],
             email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(8)]],
-            confirmPassword: ['', Validators.required],
+            passwords: this.formBuilder.group({
+                password: ['', [Validators.required, Validators.minLength(8)]],
+                confirmPassword: ['']
+            }, { validators: this.checkPasswords }),
             isHostingPolls: [false],
             is2FAEnabled: [false],
             securityQuestions: new FormArray([])
-        }, { validators: this.checkPasswords });
+        });
 
         // Initialize three empty security question fields
         for (var i = 0; i < 3; i++) {
@@ -42,15 +45,15 @@ export class SignupPageComponent implements OnInit {
     get sfa() { return this.rf.securityQuestions as FormArray; }
 
     onSubmit() {
-        this.authService.signup(this.rf.email.value, this.rf.password.value).subscribe((res: HttpResponse<any>) => {
-            console.log(res);
-            this.router.navigate(['/lists']);
-        });
+        console.log(this.rf);
+        console.log(this.registrationForm.valid);
+        // this.authService.signup(this.rf.email.value, this.rf.password.value).subscribe((res: HttpResponse<any>) => {
+        //     console.log(res);
+        //     this.router.navigate(['/lists']);
+        // });
     }
 
     private checkPasswords(group: FormGroup) {
-        const password = group.get('password').value;
-        const confirmPassword = group.get('confirmPassword').value;
-        return password === confirmPassword ? null : { notSame: true }
+        return group.get('password').value === group.get('confirmPassword').value ? null : { mismatch: true }
     }
 }
