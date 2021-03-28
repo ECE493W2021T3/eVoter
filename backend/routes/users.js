0f38b238abd2d0ca0d6b4b2ae3beda662ca6f3e6
Enson <gza060625@gmail.com>
2021-03-28 16:31:20 -0600

    Update server with User - introduce User Schema - implement User Joi check - rewrite some of User Model and Route
    Signed-off-by: Enson <gza060625@gmail.com>

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,5 @@
-const express = require("express");
-const router = express.Router();
-const { User } = require('../models/user');
+const router = require("express").Router();
+const { User, validateUser } = require('../models/user');
 const { verifySession } = require("../middleware/verifySession");
 let network = require('../services/network');
 
@@ -9,33 +8,36 @@ let network = require('../services/network');
  * Purpose: Sign up
  */
 router.post('/', (req, res) => {
-    // User sign up
 
     let body = req.body;
+
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
     let newUser = new User(body);
 
-    newUser.save().then(() => {
-        return newUser.createSession();
-    }).then((refreshToken) => {
-        // Session created successfully - refreshToken returned.
-        // now we geneate an access auth token for the user
+    newUser
+        .save()
+        .then(() => { return newUser.createSession(); })
+        .then((refreshToken) => {
+            // Session created successfully - refreshToken returned.
+            // now we geneate an access auth token for the user
+            return newUser.generateAccessAuthToken().then((accessToken) => {
+                // access auth token generated successfully, now we return an object containing the auth tokens
+                return { accessToken, refreshToken }
+            });
+        })
+        .then(async (authTokens) => {
+            // Sign up user in Blockchain and add to wallet
+            await network.registerUser(newUser._id.toString());
 
-        return newUser.generateAccessAuthToken().then((accessToken) => {
-            // access auth token generated successfully, now we return an object containing the auth tokens
-            return { accessToken, refreshToken }
-        });
-    }).then(async (authTokens) => {
-        // Sign up user in Blockchain and add to wallet
-        await network.registerUser(newUser._id.toString());
-
-        // Now we construct and send the response to the user with their auth tokens in the header and the user object in the body
-        res
-            .header('x-refresh-token', authTokens.refreshToken)
-            .header('x-access-token', authTokens.accessToken)
-            .send(newUser);
-    }).catch((e) => {
-        res.status(400).send(e);
-    })
+            // Set JWT tokens
+            res
+                .header('x-refresh-token', authTokens.refreshToken)
+                .header('x-access-token', authTokens.accessToken)
+                .send(newUser);
+            })
+        .catch((e) => { res.status(400).send(e); })
 })
 
 
@@ -44,10 +46,8 @@ router.post('/', (req, res) => {
  * Purpose: Login
  */
 router.post('/login', (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
 
-    User.findByCredentials(email, password).then((user) => {
+    User.findByCredentials(req.body.email, req.body.password).then((user) => {
         return user.createSession().then((refreshToken) => {
             // Session created successfully - refreshToken returned.
             // now we geneate an access auth token for the user
@@ -71,7 +71,6 @@ router.post('/login', (req, res) => {
         res.status(400).send(e);
     });
 })
-
 
 /**
  * GET /users/me/access-token
