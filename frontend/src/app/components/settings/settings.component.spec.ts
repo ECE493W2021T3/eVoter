0ef7ab533922a24dff86f869aa09168695715b75
Trishia Gatilogo <gatilogo@ualberta.ca>
2021-04-16 04:34:38 -0600

    settings unit tests

--- frontend/src/app/components/settings/settings.component.spec.ts
+++ frontend/src/app/components/settings/settings.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { MatDialogRef } from '@angular/material/dialog';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { imports } from 'src/app/app.imports';
+import { UserService } from 'src/app/services/user.service';
 
 import { SettingsComponent } from './settings.component';
 
@@ -8,7 +13,11 @@ describe('SettingsComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [SettingsComponent]
+            declarations: [SettingsComponent],
+            imports: imports,
+            providers: [
+                { provide: MatDialogRef, useValue: {} }
+            ]
         })
             .compileComponents();
     });
@@ -16,10 +25,47 @@ describe('SettingsComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(SettingsComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
-    // });
+    it('initialize form with 2FA setting', fakeAsync(() => {
+        initializeSetting();
+        expect(component.sf.is2FAEnabled.value).toBeTrue();
+    }));
+
+    it('submit button should be initially disabled', fakeAsync(() => {
+        initializeSetting();
+
+        let button = fixture.debugElement.query(debugEl => {
+            return debugEl.name === 'button' && debugEl.nativeElement.textContent === 'Save';
+        });
+
+        expect(button.nativeElement.disabled).toBeTrue();
+    }));
+
+    it('updating setting should enable submit button', fakeAsync(() => {
+        initializeSetting();
+
+        let button = fixture.debugElement.query(debugEl => {
+            return debugEl.name === 'button' && debugEl.nativeElement.textContent === 'Save';
+        });
+
+        expect(button.nativeElement.disabled).toBeTrue();
+
+        component.sf.is2FAEnabled.setValue(false);
+        component.settingsForm.markAsDirty();
+
+        expect(component.sf.is2FAEnabled.value).toBeFalse();
+        expect(component.settingsForm.dirty).toBeTrue();
+        // expect(button.nativeElement.disabled).toBeFalse();
+    }));
+
+    const initializeSetting = function() {
+        let userService = fixture.debugElement.injector.get(UserService);
+        spyOn(userService, "get2FAConfig").and.callFake(() => {
+            return of(true).pipe(delay(300));
+        });
+
+        fixture.detectChanges();
+        tick(300);
+    };
 });
